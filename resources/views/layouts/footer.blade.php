62657fd86cacfd08dbf8b764d77f910efcbba7f4
kwizera <emmizokwizera@gmail.com>
2023-01-05 18:02:56 +0200

    check rich text

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -43,20 +43,50 @@
 
 
 <script type="text/javascript">
+    // tinymce.init({
+    //     selector: 'textarea.tinymce-editor',
+    //     height: 200,
+    //     menubar: false,
+    //     plugins: [
+    //         'advlist autolink lists link image charmap print preview anchor',
+    //         'searchreplace visualblocks code fullscreen',
+    //         'insertdatetime media table paste code help wordcount', 'image'
+    //     ],
+    //     toolbar: 'undo redo | formatselect | ' +
+    //         'bold italic backcolor | alignleft aligncenter ' +
+    //         'alignright alignjustify | bullist numlist outdent indent | ' +
+    //         ' blocks fontfamily fontsize | bold italic underline strikethrough |' +
+    //         'removeformat | help',
+    //     tinycomments_mode: 'embedded',
+    //     tinycomments_author: 'Author name',
+    //     mergetags_list: [{
+    //             value: 'First.Name',
+    //             title: 'First Name'
+    //         },
+    //         {
+    //             value: 'Email',
+    //             title: 'Email'
+    //         },
+    //     ],
+    //     content_css: '//www.tiny.cloud/css/codepen.min.css'
+    // });
     tinymce.init({
         selector: 'textarea.tinymce-editor',
         height: 200,
-        menubar: false,
-        plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount', 'image'
-        ],
-        toolbar: 'undo redo | formatselect | ' +
-            'bold italic backcolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-        content_css: '//www.tiny.cloud/css/codepen.min.css'
+        // menubar: false,
+        plugins: 'anchor autolink charmap codesample emoticons link lists  searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough  | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [{
+                value: 'First.Name',
+                title: 'First Name'
+            },
+            {
+                value: 'Email',
+                title: 'Email'
+            },
+        ]
     });
 </script>
 
