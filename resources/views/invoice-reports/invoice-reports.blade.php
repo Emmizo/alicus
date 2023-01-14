8bdb91cf9bad1d90d0ecb50b9c0ab9dc6b560405
kwizera <emmizokwizera@gmail.com>
2023-01-14 16:53:09 +0200

    print issues solved

--- resources/views/invoice-reports/invoice-reports.blade.php
+++ resources/views/invoice-reports/invoice-reports.blade.php
@@ -19,7 +19,7 @@
 
         <div class="mt-5">
 
-            <div class="card-body ">
+            <div class="card-body " id="printData">
                 <div class=" col-lg-12 col-md-4 col-sm-3 d-print-none">
                     {{-- <div class="search">
                         <i class="fa fa-search"></i>
@@ -46,6 +46,10 @@
                             <div class="col-md-3">
                                 <button class="btn btn-primary" type="submit">Search</button>
                             </div>
+
+                            <div class="col-md-3 d-print-none">
+                                <input class="btn btn-primary" type='button' id='print-data' value='Print'>
+                            </div>
                         </div>
 
                     </form>
@@ -53,7 +57,7 @@
 
                 <fieldset class="border p-2 mt-3">
                     <legend class="float-none w-auto">Invoice</legend>
-                    <div id="print-invoice33" class="mb-2">
+                    {{-- <div id="print-invoice33" class="mb-2">
                         <div class="col-12 container font-weight-bold row">
 
                             <div class="col-md-6 ">
@@ -77,11 +81,46 @@
                             </div>
                         </div>
 
-                    </div>
+                    </div> --}}
+                    <table class=" table table-bordered certificate-table" border="1">
+                        <tbody>
+
+                            <tr>
+
+                                <td>Company: {{ $data->company_name ?? '' }}</td>
+                                <td rowspan="3">
+                                    <div class="col-md-12 ">
+                                        <img class="logo-img2 float-md-right"
+                                            src='{{ URL::asset($data->company_logo ?? 'companies_logo/no-logo.png') }}'
+                                            alt="{{ $data->company_name ?? '' }}">
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+
+
+                                <td>Phone: {{ $data->phone ?? '' }}</td>
+
+                            </tr>
+                            <tr>
+
+                                <td>Email: {{ $data->email ?? '' }}</td>
+
+                            </tr>
+                        </tbody>
+
+                    </table>
 
                     @if ($invoices->count() > 0)
-                        <div class="container mb-4 fs-5">From {{ $from }} to {{ $to }} Invoice available
-                            are bellow
+                        <div class="container mb-4 fs-5">
+                            {{ $from
+                                ? ' from ' .
+                                    $from .
+                                    ' to ' .
+                                    $to .
+                                    " Invoice available
+                                                                                    are bellow"
+                                : '' }}
                         </div>
                         <table border="1" class="table table-bordered certificate-table" width="500">
                             <tbody>
@@ -132,15 +171,17 @@
                         available
                     </div>
                     @endif
-            </div>
-            </fieldset>
+                </fieldset>
+                <footer class=" print">
+                    <strong>
+                        <div class="float-right">Logged Into As
+                            {{ Auth::user()->first_name . '  ' . Auth::user()->last_name }} on
+                            {{ date('Y-m-d H:i:s') }}</div>
+                    </strong>
 
-        </div>
-        <div class="form-row">
-            <div class="col-md-4 d-print-none">
-                <input class="btn btn-primary" type='button' id='print-data' value='Print'>
+                </footer>
             </div>
-        </div>
+
 
         </div>
 
@@ -157,13 +198,49 @@
             document.title = '{{ $data->company_name }}';
 
             function printData() {
+                var contents = document.getElementById("printData").innerHTML;
+                var frame1 = document.createElement('iframe');
+                frame1.name = "printData";
+                frame1.style.position = "absolute";
+                frame1.style.top = "-1000000px";
+                document.body.appendChild(frame1);
+                var frameDoc = (frame1.contentWindow) ? frame1.contentWindow : (frame1.contentDocument.document) ?
+                    frame1.contentDocument.document : frame1.contentDocument;
+                frameDoc.document.open();
+                frameDoc.document.write('<html><head><title></title>');
 
-                var divToPrint = document.getElementById("printData");
 
-                newWin = window.print();
+                frameDoc.document.write(
+                    ' <link href = "{{ asset('/dist/css/adminlte.min.css') }}"rel = "stylesheet" / >'
+                );
+                frameDoc.document.write(
+                    '<link href = "{{ asset('assets/css/style.css') }}"rel = "stylesheet" / > '
+                );
+                frameDoc.document.write(
+                    '<link rel="stylesheet" type="text/css" href="{{ asset('custom/sweetalert2/css/sweetalert2.min.css') }}" />'
+                );
+                frameDoc.document.write(
+                    '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">'
 
-                newWin.close();
 
+                );
+                frameDoc.document.write(
+                    '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">'
+                );
+                frameDoc.document.write(
+                    '</head><body >'
+                );
+                frameDoc.document.write(contents);
+                frameDoc.document.write(
+                    ' </body></html>'
+                );
+                frameDoc.document.close();
+                setTimeout(function() {
+                    window.frames["printData"].focus();
+                    window.frames["printData"].print();
+                    document.body.removeChild(frame1);
+                }, 100);
+                return false;
             }
 
 
